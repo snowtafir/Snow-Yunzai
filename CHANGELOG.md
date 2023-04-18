cb31f7c8475d9104601695f3f7bc8177ec29a417
🌌 <Time.Rain.Star.Sky@Gmail.com>
2023-04-18 15:12:48 +0800

    同步 Miao-Yunzai 更新

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,13 @@
+# 3.0.2
+
+* 支持协议端：ComWeChat、Telegram
+* 3.6卡池以及图像武器别名等数据更新 **@cvs**
+* 将渲染逻辑独立，支持扩展渲染器 **@ikuaki**
+
 # 3.0.1
 
-- 支持多账号，支持协议端：go-cqhttp、ComWeChat、Telegram
-- 由于完全删除了 OICQ，并且内置 `segment`，若插件缺少 OICQ，需删除 `import { segment } from "oicq"`
+* 支持多账号，支持协议端：go-cqhttp
+* 由于完全删除了 OICQ，并且内置 `segment`，若插件缺少 OICQ，需删除 `import { segment } from "oicq"`
 
 # 3.0.0
 
--- plugins/system/master.js
+++ plugins/system/master.js
@@ -25,14 +25,13 @@ export class master extends plugin {
 
     code[this.e.user_id] = randomUUID()
     logger.mark(`${logger.cyan(`[${this.e.user_id}]`)} 设置主人验证码：${logger.green(code[this.e.user_id])}`)
-    await this.reply(`账号：${this.e.user_id} 请输入验证码`, true)
-
     this.setContext("verify")
+    await this.reply(`账号：${this.e.user_id} 请输入验证码`, true)
   }
 
   async verify () {
     this.finish("verify")
-    if (this.e.msg == code[this.e.user_id]) {
+    if (this.e.msg.trim() == code[this.e.user_id]) {
       fs.writeFileSync(file, fs.readFileSync(file, "utf8").replace(/masterQQ:/g, `masterQQ:\n  - ${this.e.user_id}`), "utf8")
       await this.reply(`账号：${this.e.user_id} 设置主人成功`, true)
     } else {
