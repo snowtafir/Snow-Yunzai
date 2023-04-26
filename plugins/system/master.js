ffadc18e008cb8397f1e29227f09a9453f9eb3b6
🌌 <Time.Rain.Star.Sky@Gmail.com>
2023-04-26 11:07:18 +0800

    优化 主人设置

--- plugins/system/master.js
+++ plugins/system/master.js
@@ -17,8 +17,22 @@ export class master extends plugin {
     })
   }
 
+  edit (file, key, value) {
+    let data = fs.readFileSync(file, "utf8")
+    if (data.match(RegExp(`- "?${value}"?`)))
+      return
+    value = `${key}:\n  - "${value}"`
+    if (data.match(RegExp(`${key}:`)))
+      data = data.replace(RegExp(`${key}:`), value)
+    else
+      data = `${data}\n${value}`
+    fs.writeFileSync(file, data, "utf8")
+  }
+
   async master () {
     if (this.e.isMaster) {
+      this.edit(file, "masterQQ", this.e.user_id)
+      this.edit(file, "master", `${this.e.self_id}:${this.e.user_id}`)
       await this.reply(`账号：${this.e.user_id} 已经为主人`, true)
       return false
     }
@@ -32,7 +46,8 @@ export class master extends plugin {
   async verify () {
     this.finish("verify")
     if (this.e.msg.trim() == code[this.e.user_id]) {
-      fs.writeFileSync(file, fs.readFileSync(file, "utf8").replace(/masterQQ:/g, `masterQQ:\n  - ${this.e.user_id}`), "utf8")
+      this.edit(file, "masterQQ", this.e.user_id)
+      this.edit(file, "master", `${this.e.self_id}:${this.e.user_id}`)
       await this.reply(`账号：${this.e.user_id} 设置主人成功`, true)
     } else {
       await this.reply("验证码错误", true)
